c657698fc791095b6513c675bc622628abca7a27
Mokshika Bele <mokshika.bele1407@gmail.com>
2026-06-22 12:35:54 +0530

    Updated Java sample for Codacy

--- LibraryManagement.java
+++ LibraryManagement.java
@@ -1,9 +1,30 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LibraryManagement {
-    static ArrayList<String> books = new ArrayList<>();
 
-    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
-} 
+        Scanner scanner = new Scanner(System.in);
+
+        String book = null;
+
+        int unusedVariable = 100;
+
+        System.out.println("Enter book name:");
+        String input = scanner.nextLine();
+
+        // Incorrect String comparison
+        if (input == "Java") {
+            System.out.println("Java Book Selected");
+        }
+
+        // Possible NullPointerException
+        try {
+            System.out.println(book.length());
+        } catch (Exception e) {
+            // Empty catch block
+        }
+
+        System.out.println("Program Finished");
+    }
+}
